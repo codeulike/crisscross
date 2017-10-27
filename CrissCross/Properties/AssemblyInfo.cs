6ecf033795ebd41b6dd69f6cf4773bce76e25591
codeulike7 <ian@codeulike>
2017-10-27 15:51:16 +0000

    version update

--- CrissCross/Properties/AssemblyInfo.cs
+++ CrissCross/Properties/AssemblyInfo.cs
@@ -48,5 +48,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.9.0.1")]
+[assembly: AssemblyVersion("0.9.2.0")]
 
